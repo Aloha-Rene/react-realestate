9fcc9e0ade9f7cfbad7a861dda395bef474797fb
aloha-rene <reneonlineg1@gmail.com>
2019-09-03 22:22:47 -0400

    initial upload

---
+++ assets/js/components/data/listingsData.js
@@ -0,0 +1,85 @@
+var data = [
+	{
+		address: '20-34 grand ave',
+		city: 'Ridgewood',
+		state: 'NY',
+		rooms: 3,
+		price: 220000,
+		floorSpace: 2000,
+		extras: ['elevator', 'gym'],
+		homeType: 'Apartment',
+		image: 'https://c1.staticflickr.com/3/2073/2102740345_b2e6ee63c3_z.jpg?zz=1'
+	},
+	{
+		address: '20-24 smith ave',
+		city: 'Bronx',
+		state: 'NY',
+		rooms: 2,
+		price: 520000,
+		floorSpace: 1800,
+		extras: ['elevator', 'pool'],
+		homeType: 'Multi Home',
+		image:
+			'https://assets3.thrillist.com/v1/image/1891766/size/tmg-article_default_mobile.jpg'
+	},
+	{
+		address: '8392 cool ave',
+		city: 'Warren',
+		state: 'NJ',
+		rooms: 5,
+		price: 620000,
+		floorSpace: 7000,
+		extras: ['elevator', 'gym'],
+		homeType: 'Studio',
+		image:
+			'https://www.selecthousing.com/sites/default/files/imagecache/property_primary/sites/default/files/properties/Greensboro%20NC%20Furnished%20Apartments%20Legacy%20at%20Friendly%20Manor%20Temporary%20Housing%201.JPG'
+	},
+	{
+		address: '1400 5th street',
+		city: 'Union',
+		state: 'MA',
+		rooms: 5,
+		price: 210000,
+		floorSpace: 4000,
+		extras: ['elevator', 'pool'],
+		homeType: 'Single Home',
+		image: 'https://i.ytimg.com/vi/SSKPOv0J6x0/hqdefault.jpg'
+	},
+	{
+		address: '5591 Blvd East',
+		city: 'Wayne',
+		state: 'CA',
+		rooms: 6,
+		price: 620000,
+		floorSpace: 9000,
+		extras: ['elevator', 'gym'],
+		homeType: 'Apartment',
+		image:
+			'https://images1.apartments.com/i2/iAG3fyF1bKYk-KLk8Rfrz8j8wawH4V9ptENoiLKjy5w/117/image.jpg'
+	},
+	{
+		address: '1600 Penn Ave',
+		city: 'Orion',
+		state: 'NV',
+		rooms: 2,
+		price: 120000,
+		floorSpace: 4000,
+		extras: ['elevator', 'pool'],
+		homeType: 'House',
+		image:
+			'http://images1.aptcdn.com/i2/-OSZABcrfk1eur6iGRxPOoZ2_Gs70w8je59tKLFkBoU/117/image.jpg'
+	},
+	{
+		address: '5 Greg St',
+		city: 'LittleNeck',
+		state: 'NY',
+		rooms: 0,
+		price: 20000,
+		floorSpace: 1100,
+		extras: ['elevator', 'gym'],
+		homeType: 'Condo',
+		image: 'https://luxport.s3.amazonaws.com/91095/616987_1-EXT.jpg'
+	}
+];
+
+export default data;
